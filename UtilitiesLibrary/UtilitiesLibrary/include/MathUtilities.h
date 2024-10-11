310a9ee10d29d2312abe70be1a43120ad7fee7ca
Luis R <78448904+Starmise@users.noreply.github.com>
2024-10-11 17:50:58 -0600

    Vector 3 added
    Vector3 class added and the code from Vector2 class was optimazed. New "sqrt" method similar to sqrtNewthon added to obtain the square root using Newthon-Raphson.

--- UtilitiesLibrary/UtilitiesLibrary/include/MathUtilities.h
+++ UtilitiesLibrary/UtilitiesLibrary/include/MathUtilities.h
@@ -5,7 +5,7 @@
   * @param num Valor del que se calcula el número absoluto
   */
 float
-absoluteValue(float num) {
+abs(float num) {
   if (num < 0) {
     return num * -1;
   }
@@ -21,10 +21,29 @@ sqrtNewthon(int num) {
   float errMargin = 0.0001; //Margen permitido de error
   float estimate = 1.0; // Primera estimación
   // Se irá calculando hasta que la estimación sea cercana a la raíz cuadrada del número
-  while (absoluteValue((estimate * estimate) - num) >= errMargin) {
+  while (abs((estimate * estimate) - num) >= errMargin) {
     float quotient = num / estimate; // Cociente entre el número y la estimación actual
     float prom = (quotient + estimate) / 2.0; // Cálculo para el valor de estimación
     estimate = prom;
   }
   return estimate;
+}
+
+/*
+  * @brief Se calcula la raiz cuadrada mediante el método de Newthon-Raphson
+  * @param num Valor del que se calcula la raiz cuadrada
+  */
+inline float sqrt(float value) {
+  // No se permiten valores negativos
+  if (value < 0) {
+    return 0;
+  }
+  float x = value;
+  float y = 1.0f;
+  float epsilon = 0.00001f;
+  while (x - y > epsilon) {
+    x = (x + y) / 2.0f;
+    y = value / x;
+  }
+  return x;
 }
--- UtilitiesLibrary/UtilitiesLibrary/include/Vector3.h
+++ UtilitiesLibrary/UtilitiesLibrary/include/Vector3.h
@@ -1,95 +1,85 @@
 #pragma once
 #include "MathUtilities.h"
 
-class
-Vector2 {
+class Vector3
+{
 public:
   float x; // Valor en x del vector
   float y; // Valor en y del vector
+  float z; // Valor en z del vector
 
   /**
     * @brief Constructor por defecto que inicializa los valores "x" y "y" en 0
     */
-  Vector2() : x(0), y(0) {};
+  Vector3() : x(0), y(0), z(0) {};
 
   /**
     * @brief Constructor con parámetros para los valores "x" y "y"
     */
-  Vector2(float xNum, float yNum) : x(xNum), y(yNum) {}
+  Vector3(float xNum, float yNum, float zNum) : x(xNum), y(yNum), z(zNum) {}
 
   /**
     * @brief Destructor por defecto
     */
-  ~Vector2() = default;
+  ~Vector3() = default;
 
   /**
-    * Respecto a la sobrecarga del operadores, operator se usa para definir
-    * el operador que se va a sobrecargar. Los operandos son, primero el objeto
-    * que invoca al método y el segundo el que se pasa por el parámetro. El segundo
-    * operando llamado other en este caso, es pasado por referencia para evitar
-    * problemas. Como la operación no modifica el valor del argumento, se usa const
-    * dentro del paso por referenciaantes y como el método tampoco modifica el valor,
-    * se usa un segundo const luego del parámetro. Se usa debido a que de otra forma no
-    * es posible sumar dos clases como tal.
-   */
-
-   /**
      * @brief Sobrecarga del operador +, para sumar un vector a otro vector.
      * @param other El otro vector que se sumará.
      */
-  Vector2
-   operator+(const Vector2& other) const {
+  Vector3
+    operator+(const Vector3& other) const {
     float new_xNum = x + other.x;
     float new_yNum = y + other.y;
-    return Vector2(new_xNum, new_yNum);
+    float new_zNum = z + other.z;
+    return Vector3(new_xNum, new_yNum, new_zNum);
   }
 
   /**
      * @brief Sobrecarga del operador -, para restar un vector a otro vector.
+     * Se optimiza realizando ahora las operaciones dentro del return.
      * @param other El otro vector que se restará.
      */
-  Vector2
-  operator-(const Vector2& other) const {
-    float new_xNum = x - other.x;
-    float new_yNum = y - other.y;
-    return Vector2(new_xNum, new_yNum);
+  Vector3
+    operator-(const Vector3& other) const {
+    return Vector3(x - other.x, y - other.y, z - other.z);
   }
 
   /**
      * @brief Sobrecarga del operador *, para multiplicar un vector a otro vector.
      * @param other El otro vector que se multiplicará.
      */
-  Vector2
-  operator*(float scalar) const {
-    return Vector2(x * scalar, y * scalar);
+  Vector3
+    operator*(float scalar) const {
+    return Vector3(x * scalar, y * scalar, z * scalar);
   }
 
   /**
     * @brief Se cacula la magnitud de los valores del vector
     */
   float
-  magnitude() {
-    return sqrtNewthon(x * x + y * y);
+    magnitude() {
+    return sqrt(x * x + y * y + z * z);
   }
 
   /**
      * @brief Normalización del vector que devuelva un vector con la misma dirección
      */
-  Vector2
-  normalize() {
+  Vector3
+    normalize() {
     float mag = magnitude();
     // Si la magnitud es 0, se devuelve un vector con las coordenadas (0,0)
     if (mag == 0) {
-      return Vector2(0, 0);
+      return Vector3(0, 0, 0);
     }
-    return Vector2(x / mag, y / mag);
+    return Vector3(x / mag, y / mag, z / mag);
   }
 
   /**
     * @brief Método que devuelve un puntero a los componentes del vector
     */
   float*
-  data() {
+    data() {
     return &x;
   }
 
@@ -97,7 +87,7 @@ public:
     * @brief Método constante con un puntero a los componentes del vector sin modificarlos
     */
   const float*
-  data() const {
+    data() const {
     return &x;
   }
 
